fcc803aae4299a14ecb3f83d3920cd7e5dabf2cc
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2021-05-11 13:19:10 +0200

    Improve LIB_SPEC to use libc nano and kernel-nopatch

--- gcc/config/mips/ps2sdk.h
+++ gcc/config/mips/ps2sdk.h
@@ -1,5 +1,10 @@
 #undef  LIB_SPEC
-#define LIB_SPEC "--start-group -lc -lg -lm -lps2sdkc -lkernel --end-group"
+#define LIB_SPEC " %{newlib-nano:-lm_nano} %{!newlib-nano:-lm} \
+    --start-group \
+    %{newlib-nano:%{g:-lg_nano} %{!g:-lc_nano}} %{!newlib-nano:%{g:-lg} %{!g:-lc}}  \
+    -lps2sdkc \
+    %{kernel-nopatch:-lkernel-nopatch} %{!kernel-nopatch:-lkernel} \
+    --end-group"
 
 #undef STARTFILE_SPEC
 #define STARTFILE_SPEC "crt0.o%s crti.o%s crtbegin.o%s"
