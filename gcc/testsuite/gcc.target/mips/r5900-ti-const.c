0c0844bc0e1a2fe95f2baa88b7ddeded463b6a91
Maximus32 <rgaiser@gmail.com>
2025-12-03 19:42:06 +0100

    Fix 128bit constants (ICE)

---
+++ gcc/testsuite/gcc.target/mips/r5900-ti-const.c
@@ -0,0 +1,42 @@
+/* Test R5900 128-bit constant handling */
+/* { dg-do compile } */
+/* { dg-options "-march=r5900" } */
+/* { dg-skip-if "code quality test" { *-*-* } { "-O0" } { "" } } */
+
+#ifndef NOMIPS16
+#define NOMIPS16 __attribute__((nomips16))
+#endif
+
+typedef int int128_t __attribute__((mode(TI)));
+
+/* Test storing zero constant - should use sq $0 */
+NOMIPS16 void
+test_store_zero (int128_t *p)
+{
+  *p = 0;
+}
+/* { dg-final { scan-assembler "\tsq\t\\\$0," } } */
+
+/* Test storing non-zero constant - should use lq from constant pool then sq */
+NOMIPS16 void
+test_store_nonzero (int128_t *p)
+{
+  *p = 0x12345678;
+}
+/* { dg-final { scan-assembler "\tlq\t.*\n.*\tsq\t" } } */
+
+/* Test returning zero constant - should use por $0,$0 */
+NOMIPS16 int128_t
+test_return_zero (void)
+{
+  return 0;
+}
+/* { dg-final { scan-assembler "\tpor\t\\\$2,\\\$0,\\\$0" } } */
+
+/* Test returning non-zero constant - should use lq from constant pool */
+NOMIPS16 int128_t
+test_return_nonzero (void)
+{
+  return 0x12345678;
+}
+/* { dg-final { scan-assembler-times "\tlq\t\\\$2," 1 } } */
